0f5022b705483959ce56c158ab1a8250d406247e
Carlos <solrak_27@hotmail.com>
2025-06-26 18:05:53 +0200

    Creada la interfaz OrderRepository para el Order para la db

--- orderservice/src/main/java/com/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/orderservice/service/OrderService.java
@@ -2,14 +2,24 @@ package com.orderservice.service;
 
 
 import com.bookingservice.event.BookingEvent;
+import com.orderservice.repository.OrderRepository;
 import jakarta.persistence.criteria.Order;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
 
     @KafkaListener(topics = "booking", groupId = "order-service")
     public void orderEvent(BookingEvent bookingEvent) {
@@ -20,6 +30,7 @@ public class OrderService {
         orderRepository.saveAndFlush(order);
 
          // Update Inventory
+
     }
 
     private Order createOrder(BookingEvent bookingEvent) {
