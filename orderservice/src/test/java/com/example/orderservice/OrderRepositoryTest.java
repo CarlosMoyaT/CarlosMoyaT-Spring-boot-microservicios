2f4aee8eeb02f3eca39fc3f53decde58f5b6e0ae
Carlos <solrak_27@hotmail.com>
2025-10-01 17:56:53 +0200

    testcontainers prepared for add tests, @Container and @DynamicPropertySource added

--- orderservice/src/test/java/com/example/orderservice/OrderRepositoryTest.java
+++ orderservice/src/test/java/com/example/orderservice/OrderRepositoryTest.java
@@ -1,15 +1,33 @@
 package com.example.orderservice;
 
 
+import com.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@SpringBootTest
 public class OrderRepositoryTest {
 
     @Container
     static PostgreSQLContainer<?> pgvector = new PostgreSQLContainer<>("pgvector/pgvector:pg16");
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("server.port", pgvector::getJdbcUrl);
+        registry.add("spring.datasource.username", pgvector::getUsername);
+        registry.add("spring.datasource.password", pgvector::getPassword);
+    }
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    //tests, (se escriben como los unitarios)
 
 
 }
