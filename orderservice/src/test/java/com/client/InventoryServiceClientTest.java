b53ab97beac061b7e5cb7317cef495e7f1596c29
Carlos <solrak_27@hotmail.com>
2025-10-14 16:25:59 +0200

    Integration test, and testcontainer for inventoryServiceClient, configured Docker image, and DynamicProperties

--- orderservice/src/test/java/com/client/InventoryServiceClientTest.java
+++ orderservice/src/test/java/com/client/InventoryServiceClientTest.java
@@ -1,16 +1,50 @@
 package com.client;
 
+
 import com.orderservice.client.InventoryServiceClient;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class InventoryServiceClientTest {
+
 
     @Autowired
     private InventoryServiceClient inventoryServiceClient;
 
-    //write test about RestTemplate, ResponseEntity, etc...
+    @Container
+    static GenericContainer<?> inventoryServiceContainer = new GenericContainer<>("nginxdemos/hello:latest")
+            .withExposedPorts(80);
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        String url = String.format("http://%s:%d",
+                inventoryServiceContainer.getHost(),
+                inventoryServiceContainer.getMappedPort(80));
+        registry.add("inventory.service.url", () -> url);
+    }
+
+
+
+    //write integration test
+    @Test
+    void updateInventory_shouldCallInventoryService() {
+
+    }
+
+
+
+
+
+
+
 }
