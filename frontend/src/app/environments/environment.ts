112c56627e436bc5d758772a775b300c89d11216
Carlos <solrak_27@hotmail.com>
2026-02-04 18:01:18 +0100

    environment config created, but fix the places for render

---
+++ frontend/src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    apiUrl: //my backend url onrender.com (y cambiar cuando despliegue en render)
+    keycloak: {
+        url: //keycloak url on render.com
+        realm: 'tickets-realm',
+        clientId: 'tickets-client'
+    }
+}
