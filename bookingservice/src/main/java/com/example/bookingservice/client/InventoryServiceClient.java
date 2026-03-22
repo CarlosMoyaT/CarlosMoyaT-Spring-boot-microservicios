c1cb6b82b8243d124d837ed364b4da3077896a79
Carlos <solrak_27@hotmail.com>
2026-03-22 01:01:16 +0100

    Change RestTemplate for restClient, modern for Spring boot 3

--- bookingservice/src/main/java/com/example/bookingservice/client/InventoryServiceClient.java
+++ bookingservice/src/main/java/com/example/bookingservice/client/InventoryServiceClient.java
@@ -4,17 +4,25 @@ package com.example.bookingservice.client;
 import com.example.bookingservice.response.InventoryResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class InventoryServiceClient {
 
-    @Value("${inventory.service.url}")
-    private String inventoryServiceUrl;
+    private final RestClient restClient;
+
+    public InventoryServiceClient(@Value("${inventory.service.url}") String inventoryServiceUrl) {
+        this.restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+    }
 
     public InventoryResponse getInventory(final Long eventId) {
-        final RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(inventoryServiceUrl + "/event/" + eventId, InventoryResponse.class);
+        return restClient.get()
+                .uri("/event/{eventId}", eventId)
+                .retrieve()
+                .body(InventoryResponse.class);
     }
 
 
